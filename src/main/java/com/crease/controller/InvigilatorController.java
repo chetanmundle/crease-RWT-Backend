5478d4f1738183fddacba5d6af17caf89a92f574
Chaten Mundle <chetanmundle7875@gmail.com>
2024-05-21 13:22:08 +0530

    Login api

--- src/main/java/com/crease/controller/InvigilatorController.java
+++ src/main/java/com/crease/controller/InvigilatorController.java
@@ -1,12 +1,16 @@
 package com.crease.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,6 +57,7 @@ public class InvigilatorController
 		}
 	}
 
+//	GET all invigilator
 	@GetMapping("/get/all/invigilator")
 	public ResponseEntity<Object> getAllInvigilator()
 	{
@@ -65,6 +70,65 @@ public class InvigilatorController
 		{
 			return ResponseEntity.internalServerError().build();
 		}
+	}
+	
+//	Get one Invigilator
+	@GetMapping("/get/single/invigilator/id/{invigilatorid}")
+	public ResponseEntity<Invigilator> getOneInvigilator(@PathVariable("invigilatorid") String invigilatorid)
+	{
+		try
+		{
+			Optional<Invigilator> findById = invigilatorRepos.findById(invigilatorid);
+			if(findById.isPresent())
+			{
+				Invigilator invigilator = findById.get();
+				return ResponseEntity.ok(invigilator);
+				
+			}else {
+				return ResponseEntity.notFound().build();
+			}
+		} catch (Exception e)
+		{
+			return ResponseEntity.internalServerError().build();
+		}
+	}
+	
+//	Invigilator Login
+	@PostMapping("/post/invigilator/login")
+	public ResponseEntity<Map<String, Object>> invigilatorLogin(@RequestBody Map<String, Object> requestMap)
+	{
+		try
+		{
+			String id = (String) requestMap.get("id");
+			String pass = (String) requestMap.get("password");
+			
+			Optional<Invigilator> findByIdAndPassword = invigilatorRepos.findByIdAndPassword(id, pass);
+			
+			if(findByIdAndPassword.isPresent())
+			{
+				Invigilator invigilator = findByIdAndPassword.get();
+				
+				Map<String, Object> responseMap = new HashMap<>();
+				
+				responseMap.put("id", invigilator.getId());
+				responseMap.put("name", invigilator.getName());
+				responseMap.put("adharid", invigilator.getAdharid());
+			 	responseMap.put("phone", invigilator.getPhone());
+				responseMap.put("email", invigilator.getEmail());
+				
+				return ResponseEntity.ok(responseMap);
+				
+				
+			}else {
+				return ResponseEntity.notFound().build();
+			}
+		
+		} catch (Exception e)
+		{
+			return ResponseEntity.internalServerError().build();
+		}
+		
+		
 	}
 
 }
--- src/main/java/com/crease/doa/InvigilatorRepos.java
+++ src/main/java/com/crease/doa/InvigilatorRepos.java
@@ -9,4 +9,6 @@ import com.crease.entity.Invigilator;
 public interface InvigilatorRepos extends JpaRepository<Invigilator, String>
 {
 	Optional<Invigilator> findByAdharid(Long adharid);
+	
+	Optional<Invigilator> findByIdAndPassword(String id, String password);
 }
